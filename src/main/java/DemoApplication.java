a0ba2a136b549a2c9fffecf4d7022f5b2a90e963
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-08-30 20:04:24 +0530

    Update DemoApplication.java

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {
    }
    @RequestMapping(value = "/")
    public String hello() {
-      return "Hello World";
+      return "Hello World! Sucessful Deployment";
    }
 }
